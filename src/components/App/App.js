10c3fb7533f06b87e4a7f4f1e15bd4931fc2d99d
Tou Xiong <touyeexiong@gmail.com>
2020-01-24 15:55:00 -0600

    set up app.js and index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { connect } from 'react-redux'
+import { Hashrouter as Router, Route, Link } from 'react-router-dom'
+
+// component imports
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
+import Review from '../Review/Review';
 
 class App extends Component {
   render() {
@@ -10,7 +19,7 @@ class App extends Component {
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
-        <br/>
+        <br />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import logger from 'redux-logger'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from "react-redux";
 import registerServiceWorker from './registerServiceWorker';
 
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+    registerServiceWorker();
